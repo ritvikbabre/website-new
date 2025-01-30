98e9929d9f272e2f542e5fdaa063beecbe3ba812
Ritvik <ritvikbabre@gmail.com>
2025-01-30 23:10:19 +0530

    first_successful_deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -7,23 +7,32 @@ import Portfolio from "./components/Portfolio";
 import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 
+// Create a wrapper component to handle conditional header rendering
+const AppContent = () => {
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
+    return (
+        <div className="app">
+            {!isHomePage && <Header />}
+            <main>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/certifications" element={<Certifications />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="*" element={<div>404: Page Not Found</div>} />
+                </Routes>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
 function App() {
     return (
         <Router>
-            <div className="app">
-                <Header />
-                <main>
-                    <Routes>
-                        <Route path="/" element={<Certifications />} /> {/* Change default route to Certifications */}
-                        <Route path="/home" element={<Home />} /> {/* Add Home route */}
-                        <Route path="/portfolio" element={<Portfolio />} />
-                        <Route path="/certifications" element={<Certifications />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="*" element={<div>404: Page Not Found</div>} />
-                    </Routes>
-                </main>
-                <Footer />
-            </div>
+            <AppContent />
         </Router>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,9 +7,9 @@ function Header() {
         <header className="header">
             <nav>
                 <ul>
-                    <li><Link to="/">Certifications</Link></li>
-                    <li><Link to="/home">Home</Link></li> {/* Add Home link */}
-                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/portfolio">Resume</Link></li>
+                    <li><Link to="/certifications">Certifications</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
                 </ul>
             </nav>
