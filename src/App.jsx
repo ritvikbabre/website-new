ae3d956b1ffbae075d11dc577f4d8b27e96c9dec
Ritvik <ritvikbabre@gmail.com>
2025-01-30 21:22:33 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
@@ -9,7 +9,7 @@ import Contact from "./components/Contact";
 
 function App() {
     return (
-        <Router>
+        <Router basename="/website-new">
             <div className="app">
                 <Header />
                 <main>
